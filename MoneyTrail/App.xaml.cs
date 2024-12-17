b7c012a359ec4321318e204a3b194688f765de67
shristi_sapal <shristi.sapal@sbsolutionsnepal.com>
2024-12-17 14:59:33 +0545

    Add project files.

---
+++ MoneyTrail/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MoneyTrail
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ MoneyTrail/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MoneyTrail
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
